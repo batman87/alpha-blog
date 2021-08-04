710090ea830c14a62d5891f9b02962eb537b31a4
unknown <devadas.mallya@live.com>
2021-08-03 23:17:58 -0700

    Add bcrypt gem and the password_digest field to the User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :username, presence: true, length: {minimum: 3, maximum: 25}, uniqueness: { case_sensitive: false }
     validates :email,    presence: true, length: {maximum: 25}, format: {with: VALID_EMAIL_REGEX}
+    has_secure_password
 end
